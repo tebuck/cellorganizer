670ac9e0e63b73cf56ddd14a10835e36bb246d08
tebuck <2237442+tebuck@users.noreply.github.com>
2022-07-22 09:07:51 -0400

    Add files via upload

--- module_if_available.sh
+++ module_if_available.sh
@@ -13,7 +13,7 @@ function exit_code_to_boolean()
 
 function module_if_available()
 {
-    which modulecmd >/dev/null 2>&1 ; module_available=$(exit_code_to_boolean)
+    which modulecmd >/dev/null 2>&1 ; module_available=$(exit_code_to_boolean "$?")
     
     if (( module_available )); then
         module "$@"
